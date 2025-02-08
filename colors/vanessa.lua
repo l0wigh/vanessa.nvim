ec0248de0e1b452aab7d1dc94984f57d3241b277
L0Wigh <mathiotthomas@gmail.com>
2025-02-08 10:45:43 +0100

    (fix): Vanessa.nvim should be working now

--- colors/vanessa.lua
+++ colors/vanessa.lua
@@ -1,3 +1,6 @@
 vim.opt.background = 'dark'
 vim.g.colors_name = 'vanessa'
-require(require('lua.vanessa'))
+
+package.loaded['lush_theme.zenith'] = nil
+
+require('lush')(require('lush_theme.vanessa'))
